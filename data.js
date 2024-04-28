7950f6c357c2efecbaaaa094e0a2ed0fc1e7e5ad
romesway <romesway@gmail.com>
2024-04-28 22:08:25 +0900

    제출 전 확인

--- data.js
+++ data.js
@@ -61,18 +61,18 @@ input.addEventListener("keypress", function (event) {
 // 검색 버튼 이벤트
 const btn = document.querySelector("#btn");
 btn.addEventListener("click", (event) => {
-  // event.preventDefault(); - form일때는 이벤트 발생시 웹페이지가 새로고침 되어서 사용해야함 / or 버튼에 타입 부여 or /form대신 div 사용
+  // event.preventDefault(); - form 태그에서 버튼 타입이 submit이면 이벤트 발생시 새로고침 ->버튼에 type=button 부여 or / form대신 div 사용
   // 입력창의 밸류 값 - 대소문자 상관없이 서치하기 위해 대문자로 변환
   let text = document.getElementById("input").value.toUpperCase();
-  // 영화카드 - for문 안의 movtTitle(영화 제목)도 대문자로 변환 
+  // 영화카드 목록
   let movieCard = document.getElementsByTagName('li')
   // 배열메서드 사용위해 유사배열을 배열로 변환
   // const arrayCard = Array.from(movieCard); // map 메서드 대신 사용 가능
   const arrayCard = Object.keys(movieCard).map(el => movieCard[el]);
 
-
   // foreach 매서드를 사용해 카드 탐색
   arrayCard.forEach((element, index) => {
+    // movtTitle(영화 제목) 대문자로 변환 
     let movTitle = element.children[1].innerText.toUpperCase()
     // 검색창의 값과 영화카드의 제목이 같지 않으면 css를 통해 카드를 숨기는 id를 부여
     if (!movTitle.includes(text)) {
