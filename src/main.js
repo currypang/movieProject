810f0d1cc929206613f290978fbfc82500b22e62
romesway <romesway@gmail.com>
2024-04-30 00:20:17 +0900

    폴더 생성

--- src/main.js
+++ src/main.js
@@ -1,14 +1,18 @@
 const options = {
-  method: 'GET',
+  method: "GET",
   headers: {
-    accept: 'application/json',
-    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4NzM1ZmE2ZmQ2MTE3M2I3Nzk2ZmEyNzUyNTNmNDE4NyIsInN1YiI6IjY2MmI0YTc4OWFjNTM1MDExZDhmMmRlMCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Vk4yeQ8AiQHmqX_sadQavDK7PIaoriDP50jL6m2DQHM'
-  }
+    accept: "application/json",
+    Authorization:
+      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4NzM1ZmE2ZmQ2MTE3M2I3Nzk2ZmEyNzUyNTNmNDE4NyIsInN1YiI6IjY2MmI0YTc4OWFjNTM1MDExZDhmMmRlMCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Vk4yeQ8AiQHmqX_sadQavDK7PIaoriDP50jL6m2DQHM",
+  },
 };
 
 // tmdb 데이터 가져오기
 async function fetchMovie() {
-  const response = await fetch('https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1', options)
+  const response = await fetch(
+    "https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1",
+    options
+  );
   const jsonData = await response.json(); // jsonData.results - 영화모음[배열]
   // 영화모음(배열)을 순회하며 각 영화의 정보로 카드 만들기
   // console.log(jsonData)
@@ -19,14 +23,14 @@ async function fetchMovie() {
 }
 // 카드 눌렀을 때 영화 id 경고창으로 띄우기
 function alertCard(jsonData) {
-  const card = document.getElementsByClassName("card")
-  let arrc = Object.keys(card).map(el => card[el]);
+  const card = document.getElementsByClassName("card");
+  let arrc = Object.keys(card).map((el) => card[el]);
   arrc.forEach((element, index) => {
-    let movieID = jsonData.results[index].id
+    let movieID = jsonData.results[index].id;
     element.addEventListener("click", (element) => {
-      alert(`영화 id : ${movieID}`)
-    })
-  })
+      alert(`영화 id : ${movieID}`);
+    });
+  });
 }
 
 // 카드 생성 로직
@@ -34,8 +38,8 @@ function createCard(movie) {
   let poster = `https://image.tmdb.org/t/p/w500${movie.poster_path}`;
   let title = movie.original_title;
   let content = movie.overview;
-  let average = movie.vote_average
-  let id = movie.id
+  let average = movie.vote_average;
+  let id = movie.id;
   // 영화 카드 만들기
   let addCard = document.createElement("li");
   let addImg = document.createElement("img");
@@ -49,7 +53,7 @@ function createCard(movie) {
   addCard.appendChild(addAverage);
   // 노드 속성 설정
   addCard.setAttribute("class", "card");
-  addCard.setAttribute("id", `${id}`)
+  addCard.setAttribute("id", `${id}`);
   addImg.setAttribute("id", "img");
   addTitle.setAttribute("id", "title");
   addContent.setAttribute("id", "content");
@@ -65,7 +69,7 @@ function createCard(movie) {
 
 // 데이터 불러오기 실행
 fetchMovie();
-alertCard()
+alertCard();
 
 // 새로고침 시 커서 인풋창에 두기
 const input = document.getElementById("input");
@@ -88,23 +92,21 @@ btn.addEventListener("click", (event) => {
   // 입력창의 밸류 값 - 대소문자 상관없이 서치하기 위해 대문자로 변환
   let text = document.getElementById("input").value.toUpperCase();
   // 영화카드 목록
-  let movieCard = document.getElementsByTagName('li')
+  let movieCard = document.getElementsByTagName("li");
   // 배열메서드 사용위해 유사배열을 배열로 변환
   // const arrayCard = Array.from(movieCard); // map 메서드 대신 사용 가능
-  const arrayCard = Object.keys(movieCard).map(el => movieCard[el]);
+  const arrayCard = Object.keys(movieCard).map((el) => movieCard[el]);
 
   // foreach 매서드를 사용해 카드 탐색
   arrayCard.forEach((element, index) => {
-    // movtTitle(영화 제목) 대문자로 변환 
-    let movTitle = element.children[1].innerText.toUpperCase()
+    // movtTitle(영화 제목) 대문자로 변환
+    let movTitle = element.children[1].innerText.toUpperCase();
     // 검색창의 값과 영화카드의 제목이 같지 않으면 css를 통해 카드를 숨기는 id를 부여
     if (!movTitle.includes(text)) {
-      movieCard[index].id = 'none';
+      movieCard[index].id = "none";
       // 검색창의 값이 제목에 포함되었다면 id부여를 통해 숨김해제 - 재검색때 필요
     } else {
-      movieCard[index].id = 'some';
+      movieCard[index].id = "some";
     }
   });
-
 });
-
