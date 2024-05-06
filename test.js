20b277028e86e7c8df6fb6b998afd0bf2c958231
romesway <romesway@gmail.com>
2024-05-06 16:50:04 +0900

    feat: grid

---
+++ test.js
@@ -0,0 +1,22 @@
+let a = {};
+
+a.obj1 = { jack: { pass: 202, age: 23 } };
+
+a.obj1.mark = { pass: 202, age: 23 };
+
+let b = {
+  "0de8f260-7685-41d2-8dd9-7d2127632e21": {
+    name: "mark",
+    text: "1231313",
+    password: "11",
+  },
+  "44ca9320-13aa-4576-a7e8-67b3dda7da14": {
+    name: "tom",
+    text: "242432",
+    password: "234",
+  },
+};
+let f = "44ca9320-13aa-4576-a7e8-67b3dda7da14";
+// console.log(b);
+delete b[f];
+console.log(b);
